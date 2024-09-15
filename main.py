904c6483ade75b9f41e29caeb8d6f54203bf9b56
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-09-15 12:09:48 -0400

    add count characters function

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+    
+def get_chars_dict(text):
+    dictionary = {}
+    for chr in text:
+        chr = chr.lower()
+        if chr in dictionary:
+          dictionary[chr] += 1
+        else:
+          dictionary[chr] = 1
+    return dictionary
+        
 
 
 main()
